ee49e5b3bb1bc92ce97c1a531e5deeab0282fe57
AdauriBS <adauri.main@gmail.com>
2025-01-24 14:24:15 -0300

    update buzzer 2-2

--- Modulos/led.c
+++ Modulos/led.c
@@ -17,20 +17,6 @@ void setupLeds(void) {
     gpio_init(ledREDPin);
     gpio_set_dir(ledREDPin, GPIO_OUT);
 
-    // Configurar buzzer
-    gpio_init(buzzerPin);
-    gpio_set_dir(buzzerPin, GPIO_OUT);
-    gpio_set_function(buzzerPin, GPIO_FUNC_PWM);
-    slice_num = pwm_gpio_to_slice_num(buzzerPin);
-    pwm_set_clkdiv(slice_num, 125.0);
-    pwm_set_wrap(slice_num, 255);
-}
-
-void tocar_buzzer(void) {
-    pwm_set_gpio_level(buzzerPin, 50);
-    pwm_set_enabled(slice_num, true);
-    sleep_ms(250);
-    pwm_set_enabled(slice_num, false);
 }
 
 void controlLeds(char key) {
@@ -60,11 +46,5 @@ void controlLeds(char key) {
         if (key != 'A') gpio_put(ledBLUEPin, 0);
         if (key != 'B') gpio_put(ledGREENPin, 0);
         if (key != 'C') gpio_put(ledREDPin, 0);
-    }
-}
-
-void controlBuzzer(char key) {
-    if (key == '#') {
-        tocar_buzzer();
     }
 }
